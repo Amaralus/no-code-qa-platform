ee4571bb81710a4d9d85bf931f0bde9d5d253242
Evgenii Dolgov <navarus.n@gmail.com>
2023-11-28 21:28:58 +0300

    (#26) Added fixme

--- src/main/java/apps/amaralus/qa/platform/testcase/ExecutionActionService.java
+++ src/main/java/apps/amaralus/qa/platform/testcase/ExecutionActionService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ExecutionActionService {
 
+    // fixme later
     public ExecutionAction getById(long id) {
         return null;
     }
