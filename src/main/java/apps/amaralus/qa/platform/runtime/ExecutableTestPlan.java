de3bc8632b1a2b4cb4ed939ac3f9adc35690f955
Evgenii Dolgov <navarus.n@gmail.com>
2023-12-06 13:36:29 +0300

    (#26) Added ExecutableTestPlan

---
+++ src/main/java/apps/amaralus/qa/platform/runtime/ExecutableTestPlan.java
@@ -0,0 +1,35 @@
+package apps.amaralus.qa.platform.runtime;
+
+import apps.amaralus.qa.platform.runtime.execution.Cancelable;
+import apps.amaralus.qa.platform.runtime.execution.Executable;
+import apps.amaralus.qa.platform.runtime.execution.ExecutionGraph;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@RequiredArgsConstructor
+public class ExecutableTestPlan implements Executable, Cancelable {
+
+    private final ExecutionGraph executionGraph;
+    private final List<ExecutableTestCase> testCases;
+    private final AtomicBoolean canceled = new AtomicBoolean();
+
+    @Override
+    public void execute() {
+        if (isCanceled())
+            return;
+        executionGraph.execute();
+    }
+
+    @Override
+    public void cancel() {
+        canceled.set(true);
+        executionGraph.cancel();
+    }
+
+    @Override
+    public boolean isCanceled() {
+        return canceled.get();
+    }
+}
