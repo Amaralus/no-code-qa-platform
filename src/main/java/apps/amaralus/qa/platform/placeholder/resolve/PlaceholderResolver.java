75e723122dd0accc074e0ad3b5fb9f475cba93eb
Evgenii Dolgov <navarus.n@gmail.com>
2024-03-04 21:29:00 +0300

    (#43) Removed code for tests

--- src/main/java/apps/amaralus/qa/platform/placeholder/resolve/PlaceholderResolver.java
+++ src/main/java/apps/amaralus/qa/platform/placeholder/resolve/PlaceholderResolver.java
@@ -4,13 +4,11 @@ import apps.amaralus.qa.platform.placeholder.Placeholder;
 import apps.amaralus.qa.platform.placeholder.generate.GeneratedPlaceholderType;
 import apps.amaralus.qa.platform.placeholder.generate.PlaceholderGeneratorsProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@Service
 @RequiredArgsConstructor
 public class PlaceholderResolver {
 
-    //    private final ResolvingContext resolvingContext;
+    private final ResolvingContext resolvingContext;
     private final PlaceholderGeneratorsProvider generatorsProvider;
 
     public Object resolve(Placeholder placeholder) {
